bc84a9897a87241c19ad9f00e6908243a0cf89b6
peytonejohnsons <peyton@ejohnsons.com>
2019-01-31 20:08:43 -0600

    Added Lifter Subsystem (for real this time)

---
+++ 4778-Deep-Space/src/org/usfirst/frc/team4778/robot/subsystems/Lifter.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team4778.robot.subsystems;
+
+import org.usfirst.frc.team4778.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/*
+* Lifter.java
+* Subsystem that contains definitions for the robot lifter
+*/
+public class Lifter extends Subsystem {
+
+	
+	
+    public void initDefaultCommand() {}
+    
+    public void liftFront(boolean up) {
+	    	if(up) {
+				RobotMap.m_frontLiftSolenoid.set(DoubleSolenoid.Value.kReverse);
+			} else {
+				RobotMap.m_frontLiftSolenoid.set(DoubleSolenoid.Value.kForward);
+			}
+    }
+    
+    public void liftRear(boolean up) {
+	    	if(up) {
+				RobotMap.m_rearLiftSolenoid.set(DoubleSolenoid.Value.kReverse);
+			} else {
+				RobotMap.m_rearLiftSolenoid.set(DoubleSolenoid.Value.kForward);
+			}
+    }
+    
+    public void liftDrive(double speed, boolean forward) {
+	    	if(forward) {
+	    		RobotMap.m_lifterMotors.set(speed);
+	    	} else {
+	    		RobotMap.m_lifterMotors.set(-speed);
+	    	}
+    }
+    
+    public void stop() {
+    		RobotMap.m_lifterMotors.set(0);
+    }
+}
+
